dc9ccd4ab2c77fac8137980c17fbb8634f207768
wyylame <w1llhorn@yandex.ru>
2022-07-20 12:28:08 +0300

    add yandex map

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import {AiFillPhone, AiFillMail, AiFillClockCircle} from 'react-icons/ai'
+import { AiFillPhone, AiFillMail, AiFillClockCircle } from "react-icons/ai";
 import { BsFillPinMapFill } from "react-icons/bs";
-import './Contacts.css'
+import "./Contacts.css";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+
+const center = [55.76, 37.64];
 
 const Contacts = () => {
   return (
@@ -9,25 +12,40 @@ const Contacts = () => {
       <h1 className="contacts-title">Контактная информация</h1>
       <div className="contacts">
         <address className="phone">
-          <AiFillPhone className="contact-ico"/>
+          <AiFillPhone className="contact-ico" />
           +7 (999)-999 99 99
         </address>
         <address className="mail">
-          <AiFillMail className="contact-ico"/>
+          <AiFillMail className="contact-ico" />
           assembla@ya.ru
         </address>
         <address className="adress">
-          <BsFillPinMapFill className="contact-ico"/>
+          <BsFillPinMapFill className="contact-ico" />
           г. Москва, ул. Пушкина 127
         </address>
         <address className="time">
-          <AiFillClockCircle className="contact-ico"/>
-          Пн-Сб: с 9:00 до 18:00 
-          <br /> 
+          <AiFillClockCircle className="contact-ico" />
+          Пн-Сб: с 9:00 до 18:00
+          <br />
           Вс: Выходной
         </address>
       </div>
-      <img src="./img/map.jpg" alt="" className="map" />
+      <YMaps className="map">
+        <Map
+        width="100%"
+        height="50vh"
+          defaultState={{ center: center, zoom: 14 }}
+        >
+          <Placemark
+          key={''}
+          geometry={center}
+          options={{
+            iconLayout: "default#image",
+            iconImageSize: [50, 50],
+          }}
+        />
+        </Map>
+      </YMaps>
     </div>
   );
 };
